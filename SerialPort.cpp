1943d707386285eba51fe385bc0be8b8893ad5f2
fish <timyrviklienko@gmail.com>
2024-10-08 21:13:59 +0300

    Correct SerialPort class
    add new Q_PROPERTY and struct to hold angles, and vector.

--- SerialPort.cpp
+++ SerialPort.cpp
@@ -3,7 +3,9 @@
 #include <QMessageBox>
 
 SerialPort::SerialPort(QObject *parent)
-    : QObject{parent}{
+    : QObject{parent},
+      m_forwardAngles(90,90,90)
+{
     if(!_portConector.serialPortIsAvailable()){
         qDebug() << "Error serial port Coudn't find the arduino";
     }
@@ -33,10 +35,6 @@ void SerialPort::setTheta_2(int newTheta_2)
         return;
     m_theta_2 = newTheta_2;
     emit theta_2Changed(2,newTheta_2);
-
-
-
-
 }
 
 int SerialPort::theta_3() const
@@ -58,7 +56,7 @@ void SerialPort::writeToSerialPort(const int id, const int newTheta)
     QString whaistToString = QString::number(newTheta);
     switch (id) {
     case 1:
-         whaistToString += 'b';
+        whaistToString += 'b';
         _portConector.writeToSerialPort(whaistToString);
         qDebug() << whaistToString;
         break;
@@ -76,10 +74,74 @@ void SerialPort::writeToSerialPort(const int id, const int newTheta)
     default:
         break;
     }
+
 }
+
+void SerialPort::writeToSerialPort(){
+    _portConector.writeToSerialPort(QString::number(listAngles.data()->theta_1) + 'b');
+    _portConector.writeToSerialPort(QString::number(listAngles.data()->theta_2) + 's');
+    _portConector.writeToSerialPort(QString::number(listAngles.data()->theta_3) + 'e');
+}
+
 
 // void SerialPort::build()
 // {
 
 // }
 
+
+SerialPort::angles::angles(int t1, int t2, int t3)
+    : theta_1(t1),
+      theta_2(t2),
+      theta_3(t3) {
+}
+
+bool SerialPort::angles::operator==(const angles &newAngles)
+{
+    if(this->theta_1 == newAngles.theta_1 && this->theta_2 == newAngles.theta_2 && this->theta_3 == newAngles.theta_3)
+        return true;
+    return false;
+}
+
+SerialPort::angles SerialPort::forwardAngles() const
+{
+    return m_forwardAngles;
+}
+
+void SerialPort::setForwardAngles(const angles &newForwardAngles)
+{
+    if (m_forwardAngles == newForwardAngles)
+        return;
+    m_forwardAngles = newForwardAngles;
+
+    listAngles.push_back(m_forwardAngles);
+
+    emit forwardAnglesChanged();
+}
+
+void SerialPort::setToStructAngles(int inputTheta_1 , int inputTheta_2, int inputTheta_3)
+{
+
+    if (inputTheta_1 == -1) {
+        inputTheta_1 = listAngles.data()->theta_1;
+    }
+    if (inputTheta_2 == -1) {
+        inputTheta_2 = listAngles.data()->theta_2;
+    }
+    if (inputTheta_3 == -1) {
+        inputTheta_3 = listAngles.data()->theta_2;
+    }
+
+
+    if (valid_angles( inputTheta_1,  inputTheta_2,  inputTheta_3) != 1)
+        throw std::invalid_argument("Error: Invalid angle values.");
+    angles newAngle(inputTheta_1,inputTheta_2,inputTheta_3);
+    setForwardAngles(newAngle);
+}
+
+bool SerialPort::valid_angles(int inputTheta_1, int inputTheta_2, int inputTheta_3) const
+{
+    return (inputTheta_1 >= 0 && inputTheta_1 <= 180 &&
+            inputTheta_2 >= 0 && inputTheta_2 <= 180 &&
+            inputTheta_3 >= 0 && inputTheta_3 <= 180);
+}
--- SerialPort.h
+++ SerialPort.h
@@ -11,6 +11,7 @@ class SerialPort : public QObject
     Q_PROPERTY(int theta_2 READ theta_2 WRITE setTheta_2 NOTIFY theta_2Changed FINAL)
     Q_PROPERTY(int theta_3 READ theta_3 WRITE setTheta_3 NOTIFY theta_3Changed FINAL)
 
+    Q_PROPERTY(angles forwardAngles READ forwardAngles WRITE setForwardAngles NOTIFY forwardAnglesChanged FINAL)
 public:
     explicit SerialPort(QObject *parent = nullptr);
 
@@ -24,6 +25,20 @@ public:
     int theta_3() const;
 
 
+    struct angles
+    {
+        int theta_1;
+        int theta_2;
+        int theta_3;
+        // Constructor to initialize angles
+        angles(int t1, int t2, int t3);
+        bool operator== (const angles& newAngles);
+    };
+
+    angles forwardAngles() const;
+    void setForwardAngles(const angles &newForwardAngles);
+    void setToStructAngles(int inputTheta_1 =-1, int inputTheta_2 =-1, int inputTheta_3 =-1);
+
 public slots:
     void setTheta_1(int newTheta_1);
     void setTheta_2(int newTheta_2);
@@ -31,17 +46,24 @@ public slots:
 
 
     void writeToSerialPort(int id, int newTheta);
+    void writeToSerialPort();
+
 
 signals:
     void theta_1Changed(int id, int newTheta);
     void theta_2Changed(int id, int newTheta);
     void theta_3Changed(int id, int newTheta);
 
+    void forwardAnglesChanged();
+
 private:
+    bool valid_angles(int inputTheta_1, int inputTheta_2, int inputTheta_3) const;
+    std::vector<angles> listAngles;
     int m_theta_1;
     int m_theta_2;
     int m_theta_3;
     BackEnd _portConector;
+    angles m_forwardAngles;
 };
 
 #endif // SERIALPORT_H
